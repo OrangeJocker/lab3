a5b96f321d82d837ee1969fecb66fe1fac9fba08
crownedsilverfox <byskillcfpro@gmail.com>
2021-05-19 07:31:09 +0700

    add select a

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,6 +6,13 @@ namespace Lab3
 {
     internal class Program
     {
+        private static float SelectA()
+        {
+            Console.Write("Введите A: ");
+            float a = 0;
+            a = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+            return a;
+        }
         public static void Main(string[] args)
         {
         }
