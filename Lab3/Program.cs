263ddfc50ce6a3549902c5a2eb20f40a55d35e94
crownedsilverfox <byskillcfpro@gmail.com>
2021-05-19 07:36:06 +0700

    add do sum

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,6 +6,10 @@ namespace Lab3
 {
     internal class Program
     {
+        private static float DoDiv(float a, float b)
+        {
+            return a / b;
+        }
         public static void Main(string[] args)
         {
         }
