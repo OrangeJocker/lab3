a4b25c360c3fa86fb5a0941880ca44b28f87e5c5
crownedsilverfox <byskillcfpro@gmail.com>
2021-05-19 07:39:08 +0700

    add do sub

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,6 +6,10 @@ namespace Lab3
 {
     internal class Program
     {
+        private static float DoSub(float a, float b)
+        {
+            return a - b;
+        }
         public static void Main(string[] args)
         {
         }
